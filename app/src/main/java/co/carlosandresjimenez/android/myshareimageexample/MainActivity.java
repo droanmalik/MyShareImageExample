ee7200b5684e96bcfa3f31b304d0bf09e517efe4
Droan Malik <droanmalik@gmail.com>
2017-02-05 01:19:50 +0530

    Reduce code

--- app/src/main/java/co/carlosandresjimenez/android/myshareimageexample/MainActivity.java
+++ app/src/main/java/co/carlosandresjimenez/android/myshareimageexample/MainActivity.java
@@ -2,32 +2,22 @@ package co.carlosandresjimenez.android.myshareimageexample;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import com.squareup.picasso.Picasso;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     private static final int PICK_IMAGE_REQUEST = 0;
     private static final int SEND_MAIL_REQUEST = 1;
@@ -44,22 +34,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeViews();
+    }
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //to support rotation
+        setPic();
+    }
 
+    private void initializeViews() {
         mTextView = (TextView) findViewById(R.id.image_uri);
         mImageView = (ImageView) findViewById(R.id.image);
-
-        ViewTreeObserver viewTreeObserver = mImageView.getViewTreeObserver();
-        viewTreeObserver.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                mImageView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                mImageView.setImageBitmap(getBitmapFromUri(mUri));
-            }
-        });
-
         mFab = (FloatingActionButton) findViewById(R.id.fab);
         mFab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,15 +59,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-
-        if (mUri != null)
+        if (mUri != null) {
             outState.putString(STATE_URI, mUri.toString());
+        }
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-
         if (savedInstanceState.containsKey(STATE_URI) &&
                 !savedInstanceState.getString(STATE_URI).equals("")) {
             mUri = Uri.parse(savedInstanceState.getString(STATE_URI));
@@ -101,26 +87,16 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_sendmail) {
             sendEmail();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     public void openImageSelector() {
         Intent intent;
-
-        if (Build.VERSION.SDK_INT < 19) {
-            intent = new Intent(Intent.ACTION_GET_CONTENT);
-        } else {
-            intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-            intent.addCategory(Intent.CATEGORY_OPENABLE);
-        }
-
+        intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("image/*");
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
     }
@@ -138,91 +114,38 @@ public class MainActivity extends AppCompatActivity {
 
             if (resultData != null) {
                 mUri = resultData.getData();
-                Log.i(LOG_TAG, "Uri: " + mUri.toString());
-
                 mTextView.setText(mUri.toString());
-                mImageView.setImageBitmap(getBitmapFromUri(mUri));
+                setPic();
             }
-        } else if (requestCode == SEND_MAIL_REQUEST && resultCode == Activity.RESULT_OK) {
-
         }
     }
 
-    public Bitmap getBitmapFromUri(Uri uri) {
-
-        if (uri == null || uri.toString().isEmpty())
-            return null;
-
-        // Get the dimensions of the View
-        int targetW = mImageView.getWidth();
-        int targetH = mImageView.getHeight();
-
-        InputStream input = null;
-        try {
-            input = this.getContentResolver().openInputStream(uri);
-
-            // Get the dimensions of the bitmap
-            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-            bmOptions.inJustDecodeBounds = true;
-            BitmapFactory.decodeStream(input, null, bmOptions);
-            input.close();
-
-            int photoW = bmOptions.outWidth;
-            int photoH = bmOptions.outHeight;
-
-            // Determine how much to scale down the image
-            int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
-
-            // Decode the image file into a Bitmap sized to fill the View
-            bmOptions.inJustDecodeBounds = false;
-            bmOptions.inSampleSize = scaleFactor;
-            bmOptions.inPurgeable = true;
-
-            input = this.getContentResolver().openInputStream(uri);
-            Bitmap bitmap = BitmapFactory.decodeStream(input, null, bmOptions);
-            input.close();
-            return bitmap;
-
-        } catch (FileNotFoundException fne) {
-            Log.e(LOG_TAG, "Failed to load image.", fne);
-            return null;
-        } catch (Exception e) {
-            Log.e(LOG_TAG, "Failed to load image.", e);
-            return null;
-        } finally {
-            try {
-                input.close();
-            } catch (IOException ioe) {
-
-            }
+    public void setPic() {
+        if (mUri == null) {
+            return;
         }
+        // To use this, add compile 'com.squareup.picasso:picasso:2.5.2' to your build.gradle (Module:app)
+        Picasso.with(MainActivity.this)
+                .load(mUri)
+                .into(mImageView);
     }
 
     private void sendEmail() {
         if (mUri != null) {
             String subject = "URI Example";
-            String stream = "Hello! \n"
-                    + "Uri example" + ".\n"
-                    + "Uri: " + mUri.toString() + "\n";
+            String body =
+                    "Uri: " + mUri.toString();
 
             Intent shareIntent = ShareCompat.IntentBuilder.from(this)
                     .setStream(mUri)
                     .setSubject(subject)
-                    .setText(stream)
+                    .setText(body)
                     .getIntent();
 
             // Provide read access
             shareIntent.setData(mUri);
             shareIntent.setType("message/rfc822");
             shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-
-            if (Build.VERSION.SDK_INT < 21) {
-                shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-            } else {
-                shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
-            }
-
-
             startActivityForResult(Intent.createChooser(shareIntent, "Share with"), SEND_MAIL_REQUEST);
 
         } else {
